29ca6289fb26a759bada03cc173cee683c0e7376
Jordan Johansen & Julio Diaz <student@epicodus.com>
2015-08-12 10:45:33 -0700

    Write the roichambeau method in RockPapaerScissors

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -1,6 +1,37 @@
 <?php 
 	class RockPaperScissors 
 	{
-		
+		function rochambeau($first_input, $second_input)
+		{
+			switch($first_input) {
+				case "rock":
+					switch($second_input){
+						case "rock":
+							return "Draw";
+						case "paper":
+							return "Player 2";
+						case "scissors":
+							return "Player 1";
+					}
+				case "paper":
+					switch($second_input){
+						case "rock":
+							return "Player 1";
+						case "paper":
+							return "Draw";
+						case "scissors":
+							return "Player 2";
+					}
+				case "scissors":
+					switch($second_input){
+						case "rock":
+							return "Player 2";
+						case "paper":
+							return "Player 1";
+						case "scissors":
+							return "Draw";
+					}
+			}
+		}
 	}
 ?>
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -3,6 +3,18 @@
 	
 	class RockPaperScissorsTest extends PHPUnit_Framework_TestCase
 	{
+		function test_rock_scissors()
+		{
+			//Arrange
+		$test = new RockPaperScissors;
+		$first_input = "rock";
+		$second_input = "scissors";
 		
+		 //Act
+		 $result = $test->rochambeau($first_input, $second_input);
+		 
+		 //Assert
+		 $this->assertEquals("Player 1", $result);
+		}
 	}
 ?>
