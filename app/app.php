c694951eabc904c861c697cf1d6e231b036d2a98
Jordan Johansen & Julio Diaz <student@epicodus.com>
2015-08-12 12:19:48 -0700

    need to debug player two game

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php 
-	require_once __DIR__."../vendor/autoload.php";
-	require_once __DIR."../src/RockPaperScissors.php";
+	require_once __DIR__."/../vendor/autoload.php";
+	require_once __DIR__."/../src/RockPaperScissors.php";
 	
 	$app = new Silex\Application();
 	
@@ -10,6 +10,23 @@
 	
 	$app->get("/", function() use ($app) {
 		return $app['twig']->render('home_page.html.twig');
+	});
+	
+	$app->get("/one_player", function() use ($app) {
+		return $app['twig']->render('one_player.html.twig');
+	});
+	$app->get("/one_player_result", function() use ($app) {
+		$game = new RockPaperScissors;
+		$result = $game->rochambeau($_GET['player1'], $game->computerPlayer());
+		return $app['twig']->render('results.html.twig', array('result' => $result));
+	});
+	$app->get("/two_player", function() use ($app) {
+		return $app['twig']->render('two_player.html.twig');
+	});
+	$app->get("/two_player_result", function() use ($app) {
+		$game = new RockPaperScissors;
+		$result = $game->rochambeau($_GET['player1'], $_GET['player2']);
+		return $app['twig']->render('results.html.twig', array('result' => $result));
 	});
 	return $app;
 ?>
