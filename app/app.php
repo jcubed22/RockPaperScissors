948f7f9f6158c1a27ed23c742aca2b3a52b8d0e8
Jordan Johansen & Julio Diaz <student@epicodus.com>
2015-08-12 14:16:50 -0700

    Win at life.

--- app/app.php
+++ app/app.php
@@ -2,6 +2,11 @@
 	require_once __DIR__."/../vendor/autoload.php";
 	require_once __DIR__."/../src/RockPaperScissors.php";
 	
+	session_start();
+	if(empty($_SESSION['list_of_moves'])) {
+		$_SESSION['list_of_moves'] = array();
+	}
+	
 	$app = new Silex\Application();
 	
 	$app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -9,6 +14,7 @@
 	));
 	
 	$app->get("/", function() use ($app) {
+		RockPaperScissors::deleteAll();
 		return $app['twig']->render('home_page.html.twig');
 	});
 	
@@ -20,12 +26,18 @@
 		$result = $game->rochambeau($_GET['player1'], $game->computerPlayer());
 		return $app['twig']->render('results.html.twig', array('result' => $result));
 	});
-	$app->get("/two_player", function() use ($app) {
+	$app->get("/two_player_player1", function() use ($app) {
 		return $app['twig']->render('two_player.html.twig');
 	});
+	$app->get("/two_player_player2", function() use ($app) {
+		$game = new RockPaperScissors($_GET['player1']);
+		$game->save();
+		return $app['twig']->render('two_player_player2.html.twig');
+	});
 	$app->get("/two_player_result", function() use ($app) {
-		$game = new RockPaperScissors;
-		$result = $game->rochambeau($_GET['player1'], $_GET['player2']);
+		$game2 = RockPaperScissors::getAll()[0];
+		$game2->setPlayer2($_GET['player2']);
+		$result = $game2->rochambeau($game2->getPlayer1(), $game2->getPlayer2());
 		return $app['twig']->render('results.html.twig', array('result' => $result));
 	});
 	return $app;
--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -1,6 +1,35 @@
 <?php 
 	class RockPaperScissors 
 	{
+		private $player1;
+		private $player2;
+		
+		function __construct($player1= "", $player2 = "")
+		{
+			$this->player1 = $player1;
+			$this->player2 = $player2;
+		}
+		
+		function setPlayer1($new_move)
+		{
+			$this->player1 = $new_move;
+		}
+		
+		function getPlayer1()
+		{
+			return $this->player1;
+		}
+		
+		function setPlayer2($new_move)
+		{
+			$this->player2 = $new_move;
+		}
+		
+		function getPlayer2()
+		{
+			return $this->player2;
+		}
+		
 		function rochambeau($first_input, $second_input)
 		{
 			switch($first_input) {
@@ -45,6 +74,21 @@
 				case 2:
 					return "scissors";
 			}
+		}
+		
+		function save()
+		{
+			array_push($_SESSION['list_of_moves'], $this);
+		}
+		
+		static function getAll()
+		{
+			return $_SESSION['list_of_moves'];
+		}
+		
+		static function deleteAll()
+		{
+			$_SESSION['list_of_moves'] = array();
 		}
 	}
 ?>
