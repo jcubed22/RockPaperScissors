819d9ec79c2e9560cfd8ad70561ad117ce18bca3
Jordan Johansen & Julio Diaz <student@epicodus.com>
2015-08-12 11:06:25 -0700

    Tests written

--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -5,7 +5,7 @@
 	{
 		function test_rock_scissors()
 		{
-			//Arrange
+		//Arrange
 		$test = new RockPaperScissors;
 		$first_input = "rock";
 		$second_input = "scissors";
@@ -15,6 +15,61 @@
 		 
 		 //Assert
 		 $this->assertEquals("Player 1", $result);
+		}
+		
+		function test_rock_paper()
+		{
+			//Arrange
+			$test = new RockPaperScissors;
+			$first_input = "rock";
+			$second_input = "paper";
+			
+			//Act
+			$result = $test->rochambeau($first_input, $second_input);
+			
+			//Assert
+			$this->assertEquals("Player 2", $result);
+		}
+		
+		function test_rock_rock()
+		{
+			//Arrange
+			$test = new RockPaperScissors;
+			$first_input = "rock";
+			$second_input = "rock";
+			
+			//Act
+			$result = $test->rochambeau($first_input, $second_input);
+			
+			//Assert
+			$this->assertEquals("Draw", $result); 
+		}
+		
+		function test_paper_rock()
+		{
+			//Arrange
+			$test = new RockPaperScissors;
+			$first_input = "paper";
+			$second_input = "rock";
+			
+			//Act
+			$result = $test->rochambeau($first_input, $second_input);
+			
+			//Assert
+			$this->assertEquals("Player 1", $result);
+		}
+		function test_scissor_rock()
+		{
+			//Arrange
+			$test = new RockPaperScissors;
+			$first_input = "scissors";
+			$second_input = "rock";
+			
+			//Act
+			$result = $test->rochambeau($first_input, $second_input);
+			
+			//Assert
+			$this->assertEquals("Player 2", $result);
 		}
 	}
 ?>
